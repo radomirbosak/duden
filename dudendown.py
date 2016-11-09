b0972111af0ea905ef4ae01ae50e2534e09e8e30
Radomír Bosák <radomir.bosak@gmail.com>
2016-11-09 22:43:15 +0100

    Put the core code under function main()
    This way one can import the code without running it.

--- dudendown.py
+++ dudendown.py
@@ -80,77 +80,82 @@ def meaning_struct_from_li(li):
     return mean
 
 
-word = 'Loeffel'
-if len(sys.argv) > 1:
-    word = sys.argv[1]
-url = 'http://www.duden.de/rechtschreibung/{word}'.format(word=word)
+def main():
+    word = 'Loeffel'
+    if len(sys.argv) > 1:
+        word = sys.argv[1]
+    url = 'http://www.duden.de/rechtschreibung/{word}'.format(word=word)
 
-print(word)
-print("=" * len(word))
+    print(word)
+    print("=" * len(word))
 
-page = requests.get(url)
-if page.status_code == 404:
-    print("not found")
-    sys.exit()
+    page = requests.get(url)
+    if page.status_code == 404:
+        print("not found")
+        sys.exit()
 
-soup = BeautifulSoup(page.text, "html.parser")
-nadpis = soup.h1.get_text().replace('\xad', '')
+    soup = BeautifulSoup(page.text, "html.parser")
+    nadpis = soup.h1.get_text().replace('\xad', '')
 
-smpage = dict()
-secs = soup.findAll('section')
+    smpage = dict()
+    secs = soup.findAll('section')
 
-word = soup.h1.get_text().replace('\xad', '')
-smpage = {
-    sec.h2.get_text().split()[0]: sec for sec in secs if sec.h2
-}
+    word = soup.h1.get_text().replace('\xad', '')
+    smpage = {
+        sec.h2.get_text().split()[0]: sec for sec in secs if sec.h2
+    }
 
-try:
-    wortart = soup.findAll('strong', {"class": "lexem"})[0].get_text()
-    print(wortart)
-    print()
-except:
-    pass
+    try:
+        wortart = soup.findAll('strong', {"class": "lexem"})[0].get_text()
+        print(wortart)
+        print()
+    except:
+        pass
 
-# 1. Meaning overview
-meaning_section = smpage[Sections.meaning_overview.value]
+    # 1. Meaning overview
+    meaning_section = smpage[Sections.meaning_overview.value]
 
-mean_over_array = []
-mean_over_array = recursively_extract(meaning_section, meaning_fun)
+    mean_over_array = []
+    mean_over_array = recursively_extract(meaning_section, meaning_fun)
 
 
-# 2. Synonyms
-syn_section = smpage[Sections.synonyms.value]
+    # 2. Synonyms
+    syn_section = smpage[Sections.synonyms.value]
 
-syn_array = recursively_extract(syn_section, extract_synonym_from_li)
+    syn_array = recursively_extract(syn_section, extract_synonym_from_li)
 
-# 3. Meanings
-try:
-    meanings = smpage[Sections.meanings.value]
+    # 3. Meanings
+    try:
+        meanings = smpage[Sections.meanings.value]
 
-    mean_cats = ['Gebrauch', 'Grammatik', 'Text', 'Beispiel', 'Beispiele']
+        mean_cats = ['Gebrauch', 'Grammatik', 'Text', 'Beispiel', 'Beispiele']
 
-    means_array = recursively_extract(
-        meanings, meaning_struct_from_li, maxdepth=2)
-except KeyError:
-    means_array = []
+        means_array = recursively_extract(
+            meanings, meaning_struct_from_li, maxdepth=2)
+    except KeyError:
+        means_array = []
 
-# 4. Letters
+    # 4. Letters
 
-sec_letters = smpage[Sections.letters.value]
+    sec_letters = smpage[Sections.letters.value]
 
-letters_array = dict()
-for name, ul in zip(["before", "after"], sec_letters.find_all('ul')):
-    letters_array[name] = \
-        [li.a.get_text() for li in ul.find_all('li')]
+    letters_array = dict()
+    for name, ul in zip(["before", "after"], sec_letters.find_all('ul')):
+        letters_array[name] = \
+            [li.a.get_text() for li in ul.find_all('li')]
 
-# 5. Correct writing
+    # 5. Correct writing
 
-sec_writing = smpage[Sections.correct_writing.value]
-writing_dict = dict()
+    sec_writing = smpage[Sections.correct_writing.value]
+    writing_dict = dict()
 
-sec_writing.header.extract()
-for div in sec_writing.find_all('div'):
-    parts = div.get_text().split(':')
-    writing_dict[parts[0]] = parts[1].strip()
+    sec_writing.header.extract()
+    for div in sec_writing.find_all('div'):
+        parts = div.get_text().split(':')
+        writing_dict[parts[0]] = parts[1].strip()
 
-print_meaning(mean_over_array)
+    print_meaning(mean_over_array)
+
+
+if __name__ == '__main__':
+    main()
