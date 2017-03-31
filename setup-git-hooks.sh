1d5a04d597b0b7b0b62130082ae172a58e69010d
Radomír Bosák <radomir.bosak@gmail.com>
2017-03-31 19:37:28 +0200

    Remove git hooks remains

--- setup-git-hooks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for hook in git-hooks/*;do
-    echo "Setting up git hook for $hook"
-    basename=$(basename $hook)
-    chmod +x $hook
-    ln -sf ../../${hook} .git/hooks/$basename
-done
-echo "Git hooks setup complete"
