c05f50c0560d26241585916e731a820868590b8a
Radomír Bosák <radomir.bosak@gmail.com>
2017-03-31 22:35:01 +0200

    Bump version to 0.10.0

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='duden',
-    version='0.9.0',
+    version='0.10.0',
     packages=['duden'],
     description='CLI-based german dictionary',
     author='Radomír Bosák',
     author_email='radomir.bosak@gmail.com',
-    url='https://github.com/radomirbosak/duden-down',
-    download_url='https://github.com/radomirbosak/duden-down/archive/'
-                 '0.9.0.tar.gz',
+    url='https://github.com/radomirbosak/duden',
+    download_url='https://github.com/radomirbosak/duden/archive/'
+                 '0.10.0.tar.gz',
     keywords=['duden', 'duden.de', 'dictionary', 'cli', 'word'],
     entry_points={
         'console_scripts': [
