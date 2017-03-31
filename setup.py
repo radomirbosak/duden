36fd1f7b68092e5dc247f39b8c20fdfa4f627649
Radomír Bosák <radomir.bosak@gmail.com>
2017-03-31 20:20:02 +0200

    Add setup.py installation
    Fixes #6

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='duden',
+    version='0.1.0',
+    packages=['duden'],
+    description='CLI-based german dictionary',
+    author='Radomír Bosák',
+    author_email='radomir.bosak@gmail.com',
+    url='https://github.com/radomirbosak/duden-down',
+    download_url='https://github.com/radomirbosak/duden-down/archive/'
+                 '0.1.0.tar.gz',
+    keywords=['duden', 'duden.de', 'dictionary', 'cli', 'word'],
+    entry_points={
+        'console_scripts': [
+            'duden = duden.main:main'
+        ]
+    },
+    install_requires=[
+        "requests",
+        "beautifulsoup4",
+    ]
+)
