8fff55e518472a62e4e0e015a2ec6e92e047bd69
Radomír Bosák <radomir.bosak@gmail.com>
2020-06-06 18:28:03 +0900

    Move grammar constants to a separate file

--- duden/__init__.py
+++ duden/__init__.py
@@ -21,9 +21,9 @@ __all__ = [
 
 from .main import get, search
 
-from .main import (SINGULAR, PLURAL,
-                   PRASENS, PRATERITUM,
-                   INDIKATIV, IMPERATIV, KONJUKTIV_1, KONJUKTIV_2,
-                   PARTIZIP_1, PARTIZIP_2, INFINITIV_MIT_ZU,
-                   PERSON_1, PERSON_2, PERSON_3,
-                   NOMINATIV, GENITIV, DATIV, AKKUSATIV)
+from .constants import (SINGULAR, PLURAL,
+                        PRASENS, PRATERITUM,
+                        INDIKATIV, IMPERATIV, KONJUKTIV_1, KONJUKTIV_2,
+                        PARTIZIP_1, PARTIZIP_2, INFINITIV_MIT_ZU,
+                        PERSON_1, PERSON_2, PERSON_3,
+                        NOMINATIV, GENITIV, DATIV, AKKUSATIV)
