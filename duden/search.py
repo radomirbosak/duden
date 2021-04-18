e0305cc136e9e00e5a953af385439f6029f17052
Radomír Bosák <radomir.bosak@gmail.com>
2021-04-19 08:54:00 +0900

    Handle error when cache is corrupt
    Fixes: #130

--- duden/search.py
+++ duden/search.py
@@ -52,7 +52,7 @@ def cached_response(prefix=''):
                 try:
                     with gzip.open(full_path, 'rt', encoding='utf8') as f:
                         return f.read()
-                except FileNotFoundError:
+                except (FileNotFoundError, gzip.BadGzipFile, IOError):
                     pass
 
             result = func(cache_key, **kwargs)
