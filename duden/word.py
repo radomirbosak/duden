2ad02cbffd685417d19c70a9cffdb7075825b2fb
VIEWVIEWVIEW <39219018+VIEWVIEWVIEW@users.noreply.github.com>
2021-10-10 20:18:30 +0200

    Reusing lear_text function from common.py
    https://github.com/radomirbosak/duden/pull/147#discussion_r725599912
    
    Co-authored-by: Radomír Bosák <radomir.bosak@gmail.com>

--- duden/word.py
+++ duden/word.py
@@ -58,7 +58,7 @@ class DudenWord():
         title_element = self.soup.find('span', {"class": "lemma__main"})
         if title_element is not None:
             # remove soft hyphens "\xad" and return
-            return title_element.get_text().replace('\xad', '').strip() 
+            return clear_text(title_element.get_text())
         
         #  if the title_element does not exist, we fall back to the old method
         if self.part_of_speech is not None and 'Substantiv' not in self.part_of_speech:
