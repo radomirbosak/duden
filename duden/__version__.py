d2cdc8df1d12f911f82b92fdaba3d22de9cae73e
Radomír Bosák <radomir.bosak@gmail.com>
2022-09-17 17:05:05 +0900

    Migrate package management from pipenv to poetry
    Also introduce pyproject.toml, the latest standard.
    
    Fixes: #154

--- duden/__version__.py
+++ duden/__version__.py
@@ -2,4 +2,9 @@
 """
 Contains module version constant
 """
-__version__ = '0.17.0'
+import pkg_resources
+
+try:
+    __version__ = pkg_resources.get_distribution('duden').version
+except Exception:
+    __version__ = 'unknown'
