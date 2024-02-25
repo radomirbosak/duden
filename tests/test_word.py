02f01d5ec350a432f7532c27d818a338503778f0
Radomír Bosák <radomir.bosak@gmail.com>
2024-02-25 19:11:46 +0900

    Improve synonym splitting unit tests

--- tests/test_word.py
+++ tests/test_word.py
@@ -6,3 +6,7 @@ from duden.word import split_synonyms
 def test_split_synonyms():
     """Test one-line list splitting"""
     assert split_synonyms("") == [""]
+    assert split_synonyms("a, b ,c") == ["a", "b", "c"]
+
+    expected = ["a", "b (b, c)", "d (d, e, f) g", "h"]
+    assert split_synonyms("a, b (b, c); d (d; e, f) g, h") == expected
